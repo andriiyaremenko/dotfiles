c03299c01fd77d25081d51e272b8d768083486ab
Andrii Yaremenko <yarrem@gmail.com>
2019-03-18 00:17:01 +0200

    emacs stable

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,3 +6,15 @@
 (require 'hooks_my)
 (require 'keybindings_my)
 (require 'theme_my)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (exec-path-from-shell el-get))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- .emacs.d/settings/theme_my.el
+++ .emacs.d/settings/theme_my.el
@@ -1,4 +1,6 @@
-;; theme
+;;theme
+(add-to-list 'load-path "~/.emacs.d/el-get/darktooth-theme")
+(require 'darktooth-theme)
 (load-theme 'darktooth t)
 
-(provide 'theme_ny)
+(provide 'theme_my)

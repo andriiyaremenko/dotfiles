b2bd84fff4a46b8250e218c981921254da85b138
Andrii Yaremenko <yarrem@gmail.com>
2022-04-15 12:46:46 +0300

    nvim status-line tweak

--- .config/nvim/lua/plugins/settings.lua
+++ .config/nvim/lua/plugins/settings.lua
@@ -98,7 +98,7 @@ require 'lualine'.setup {
     options = {
         icons_enabled = true,
         theme = 'auto',
-        component_separators = { left = '╲', right = '╱' },
+        component_separators = { left = '╱', right = '╲' },
         section_separators = { left = '', right = '' },
         disabled_filetypes = { 'fern' },
         always_divide_middle = true,

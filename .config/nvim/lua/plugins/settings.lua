9ba6c2218f8b4f11bcd47cf28e4d0a2962ca373f
Andrii Yaremenko <yarrem@gmail.com>
2022-08-02 15:58:19 +0300

    nvim turns off floating window spellcheck

--- .config/nvim/lua/plugins/settings.lua
+++ .config/nvim/lua/plugins/settings.lua
@@ -161,7 +161,7 @@ configs.setup {
 -------------------------------------------------------------------------------------------------------------------------------------
 -- 2: Spellcheck displayed words in buffer. Fast and dynamic. The waiting time
 -- depends on the setting of CursorHold `set updatetime=1000`.
-vim.g.spelunker_check_type = 2
+vim.g.spelunker_highlight_type = 2
 
 -----                                                    VIM-DELVE                                                              -----
 -------------------------------------------------------------------------------------------------------------------------------------

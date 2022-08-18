a467b947e100afe490d52213d12c89bad3f45eed
Andrii Yaremenko <yarrem@gmail.com>
2022-08-18 20:12:50 +0300

    finnaly dropping alacritty, adds wezterm instead

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,80 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+return {
+    font = wezterm.font(
+        'JetBrains Mono',
+        { weight = 'DemiBold', stretch = 'ExtraCondensed' }
+    ),
+    color_scheme = 'nordfox',
+    font_size = 13,
+    default_cursor_style = 'BlinkingBlock',
+    cursor_blink_rate = 800,
+    enable_tab_bar = false,
+    keys = {
+        -- Turn off the default CMD-m Hide action, allowing CMD-m to
+        -- be potentially recognized and handled by the tab
+        { key = 'LeftArrow', mods = 'SHIFT|CTRL', action = 'DisableDefaultAssignment' },
+        { key = 'RightArrow', mods = 'SHIFT|CTRL', action = 'DisableDefaultAssignment' },
+    },
+    mouse_bindings = { -- TODO: this stupid thing still does not work (links would not open)... need to find out why later
+        -- Ctrl-click will open the link under the mouse cursor
+        {
+            event = { Up = { streak = 1, button = 'Left' } },
+            mods = 'CTRL',
+            action = wezterm.action.OpenLinkAtMouseCursor,
+        },
+        -- Disable the 'Down' event of CTRL-Click to avoid weird program behaviors
+        {
+            event = { Down = { streak = 1, button = 'Left' } },
+            mods = 'CTRL',
+            action = act.Nop,
+        },
+    },
+    hyperlink_rules = {
+        -- Linkify things that look like URLs and the host has a TLD name.
+        -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+        {
+            regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
+            format = '$0',
+        },
+
+        -- linkify email addresses
+        -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+        {
+            regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+            format = 'mailto:$0',
+        },
+
+        -- file:// URI
+        -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+        {
+            regex = [[\bfile://\S*\b]],
+            format = '$0',
+        },
+
+        -- Linkify things that look like URLs with numeric addresses as hosts.
+        -- E.g. http://127.0.0.1:8000 for a local development server,
+        -- or http://192.168.1.1 for the web interface of many routers.
+        {
+            regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+            format = '$0',
+        },
+
+        -- Make task numbers clickable
+        -- The first matched regex group is captured in $1.
+        {
+            regex = [[\b[tT](\d+)\b]],
+            format = 'https://example.com/tasks/?t=$1',
+        },
+
+        -- Make username/project paths clickable. This implies paths like the following are for GitHub.
+        -- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+        -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+        -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+        {
+            regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+            format = 'https://www.github.com/$1/$3',
+        },
+    },
+}
